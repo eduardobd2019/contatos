4ab290d27ffdf432a229a1e84ff3eaa535b94a3e
eduardobd2019 <49658345+eduardobd2019@users.noreply.github.com>
2019-11-01 19:28:11 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Classe de conexao com o banco de dados
+ */
+class Conexao{
+
+    // Nome do usuario do servidor de banco de dados
+    private $user = 'id9069320_bd2019';
+    // Senha do banco de dados
+    private $password = '2019bd2019';
+    // Nome do banco de dados para se conectar
+    private $database = 'id9069320_bd2019';
+    // Host do servidor de banco de dados
+    private $host = 'localhost';    // 000webhost //usar localhost //mysql.hostinger.com.br
+    // Obejto de conexao PDO
+    public $con;  
+
+    /**
+     * Cria a conexao quando a classe e instanciada, 
+     caso ocorra erro e retornado o erro
+     */
+    public function __construct(){
+        try{
+            $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->database.'',$this->user, 
+            $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
+            return $this->conn;
+        }catch(PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
+    }
+
+}
+
+?>
