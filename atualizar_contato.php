f34bfe7980eed31cce3ed56164642bfcc7b3341d
eduardobd2019 <49658345+eduardobd2019@users.noreply.github.com>
2019-11-08 19:29:47 -0300

    Create atualizar_contato.php

---
+++ atualizar_contato.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once('conexao.php');
+
+$conexao = new Conexao();
+
+$id = $_POST['id'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+
+$smtp = $conexao->conn->prepare('UPDATE usuarios SET nome = :nome, email = :email, senha = :senha WHERE id = :id');
+
+$smtp->execute(array(
+	'nome' => $nome,
+	'email' => $email,
+	'senha' => $senha,
+	'id' => $id )
+	
+);    
+
+ //Previne que códigos maliciosos SQL possam apagar dados do banco.
+
+if ($smtp){
+
+	echo"Atualizado com Sucesso!";
+
+}else{
+	
+	echo"Error";
+}
+
+?>
